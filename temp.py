aabebd40f1fc7dae86ef0fc31f7758bd8dadd11b
awalikar886 <awalikar886@gmail.com>
2025-10-23 15:03:51 +0530

    temperature

--- temp.py
+++ temp.py
@@ -3,3 +3,10 @@ if t<20:s="Cold"
 elif t<=30:s="Normal" 
 else:s="Hot"
 print(f"Temp:{t}°C Status:{s}")
+
+
+t=float(input("Enter °C: "))
+ if t<20:s="Cold" 
+ elif t<=30:s="Normal" 
+ else:s="Hot" f=(t*9/5)+32
+print(f"Temp:{t}°C Status:{s} Fahrenheit:{f}°F")
